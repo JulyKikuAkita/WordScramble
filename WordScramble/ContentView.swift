e488d51e54fa277dd849728cc65e22dbc5b5898b
JulyKikuAkita <b92701105@gmail.com>
2021-09-26 13:27:02 -0700

    Challenge tasks for word scramble

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -13,6 +13,11 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
 
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    @State private var score = 0
+
     var body: some View {
         NavigationView {
             VStack {
@@ -25,8 +30,16 @@ struct ContentView: View {
                     Label($0, systemImage: "\($0.count).circle")
                 }
                 .listStyle(GroupedListStyle())
+
+                Text("Score: \(score)")
             }
-        }.navigationTitle(rootWord)
+            .navigationBarTitle(rootWord)
+            .navigationBarItems(leading: Button("New Game", action: startGame))
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+        }
     }
 
     func addNewWord() {
@@ -35,11 +48,91 @@ struct ContentView: View {
             return
         }
 
+        guard isOriginal(word: answer) else {
+            minusPoints()
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            minusPoints()
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            minusPoints()
+            wordError(title: "Word not possible", message: "That isn't a real word.")
+            return
+        }
+
+        guard isNotCheating(word: answer) else {
+            minusPoints()
+            wordError(title: "AI dectects cheating", message: "Don't use the same word or letters")
+            return
+        }
+
         usedWords.insert(answer, at: 0)
         newWord = ""
+        addPoints()
+
     }
 
+    func startGame() {
+        if let startWroldsUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try?
+                String(contentsOf: startWroldsUrl) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                usedWords = []
+                return
+            }
+        }
 
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tmpWord = rootWord.lowercased()
+        for letter in word {
+            if let pos = tmpWord.firstIndex(of: letter) {
+                tmpWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return  misspelledRange.location == NSNotFound
+    }
+
+    func isNotCheating(word: String) -> Bool {
+        word.count != 1 && word != rootWord.lowercased()
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+
+    func minusPoints() {
+        score -= 5
+    }
+
+    func addPoints() {
+        score += 25
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
