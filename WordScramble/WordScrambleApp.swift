1bfa1e85cb3e6dd92bc91df308db024f043fd0b4
JulyKikuAkita <b92701105@gmail.com>
2021-09-25 22:48:24 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ifang Lee on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
