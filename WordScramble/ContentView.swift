b166237b5646a6ee64d9b9d5552955d3c0f09cf0
JulyKikuAkita <b92701105@gmail.com>
2021-09-25 23:44:18 -0700

    WordScramble - string api

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Nancy Pelosi", "Alex Padilla", "Dianne Feinsten", "Gavin Newsom"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            // spell checker API
+            let word = "swift"
+            let checker = UITextChecker()
+            let range = NSRange(location: 0, length: word.utf16.count)
+            let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+            let allGood = misspelledRange.location == NSNotFound
+
+            //string API
+            let input = """
+                金
+                蓬
+                萊
+            """
+            let letters = input.components(separatedBy: "\n")
+            let letter = letters.randomElement()
+            let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+
+            Section(header: Text("People")) {
+                ForEach(people, id:\.self) { person in
+                    Text("\(person)")
+                }
+            }
+        }
+        .listStyle(GroupedListStyle())
     }
 }
 
@@ -18,4 +42,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+
 }
